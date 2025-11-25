936bdaedcba65fdafdf8e9d2e51dd4514a72c714
thasnitr <thasnitr11@gmail.com>
2025-11-25 11:24:27 +0530

    Add files via upload

---
+++ co3/Area.py
@@ -0,0 +1,23 @@
+from Graphics.RectFunctions import *
+from Graphics.CirFunctions import *
+from Graphics.DGraphics.SphereFunctions import *
+from Graphics.DGraphics.CuboidFunctions import *
+
+l = int(input("Enter length: "))
+b = int(input("Enter breadth: "))
+print("Rectangle Area =", RectArea(l, b))
+print("Rectangle Perimeter =", RectPerimeter(l, b))
+
+r = int(input("Enter radius of Circle: "))
+print("Circle Area =", CirArea(r))
+print("Circle Perimeter =", CirPerimeter(r))
+
+r = int(input("Enter radius of Sphere: "))
+print("Sphere Area =", SpArea(r))
+print("Sphere Volume =", SpPerimeter(r))
+
+l = int(input("Enter cuboid length: "))
+b = int(input("Enter cuboid breadth: "))
+h = int(input("Enter cuboid height: "))
+print("Cuboid Area =", CubArea(l, b, h))
+print("Cuboid Perimeter =", CubPerimeter(l, b, h))
---
+++ co3/Graphics/DGraphics/SphereFunctions.py
@@ -0,0 +1,5 @@
+from math import pi
+def SpArea(r):
+    return 4*pi*r*r
+def SpPerimeter(r):
+    return (4/3)*pi*r*r*r
